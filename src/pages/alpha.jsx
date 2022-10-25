ee11d2b7228a9b3582b85d30b7bdd2bb9d978532
Viktor Holta <viktor.holta@outlook.com>
2022-10-25 13:44:49 +0200

    NEW: add customisation options to alpha

--- src/pages/alpha.jsx
+++ src/pages/alpha.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 
 import "./alpha.scss";
 import johnSmithImage from "../images/johnsmith.jpg";
 
 function AlphaLayout() {
+  const [ layout, setLayout ] = useState("right");
+  const [ theme, setTheme ] = useState("light");
+  const [ language, setLanguage ] = useState("eng");
+
   return (
-    <div className="alpha right">
+    <div className={`alpha ${layout} ${theme}`}>
       <aside>
         <h1 className="header">Layout α</h1>
         <div className="sticky">
           <Link to=".">Home</Link>
           <Link to="./contact">Contact</Link>
+
+          <hr />
+
+          <select defaultValue={layout} onChange={(e) => setLayout(e.target.value)}>
+            <option value="right">Right layout</option>
+            <option value="left">Left layout</option>
+          </select>
+          <select defaultValue={theme} onChange={(e) => setTheme(e.target.value)}>
+            <option value="light">Light theme</option>
+            <option value="dark">Dark theme</option>
+          </select>
+          <select defaultValue={language} onChange={(e) => setLanguage(e.target.value)}>
+            <option value="eng">English</option>
+            <option value="swe">Swedish</option>
+            <option value="tok">Toki pona</option>
+          </select>
         </div>
       </aside>
 
@@ -84,7 +104,7 @@ function Contact() {
             <img alt="John Smith" src={johnSmithImage} />
             <div>
               <h3 className="name">John Smith</h3>
-              <h5 className="email"><a>john.smith@gmail.com</a></h5>
+              <h5 className="email"><a href="mailto:none">john.smith@gmail.com</a></h5>
               <p>This is John Smith.</p>
             </div>
           </section>
@@ -92,7 +112,7 @@ function Contact() {
             <img alt="John Smith" src={johnSmithImage} />
             <div>
               <h3 className="name">John Smith</h3>
-              <h5 className="email"><a>also.john.smith@gmail.com</a></h5>
+              <h5 className="email"><a href="mailto:none">also.john.smith@gmail.com</a></h5>
               <p>This is also John Smith.</p>
             </div>
           </section>
@@ -100,7 +120,7 @@ function Contact() {
             <img alt="John Smith" src={johnSmithImage} />
             <div>
               <h3 className="name">John Smith</h3>
-              <h5 className="email"><a>also.john.smith.but.with.a.way.longer.address@gmail.com</a></h5>
+              <h5 className="email"><a href="mailto:none">also.john.smith.but.with.a.way.longer.address@gmail.com</a></h5>
               <p>This is also John Smith.</p>
             </div>
           </section>
